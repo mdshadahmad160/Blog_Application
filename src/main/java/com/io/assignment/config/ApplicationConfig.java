72eae042616c1b241c547256e25f32ce7e01dc45
Md Shad Ahmad <136076679+mdshadahmad160@users.noreply.github.com>
2024-10-28 16:36:54 +0530

    updated_App_config

--- src/main/java/com/io/assignment/config/ApplicationConfig.java
+++ src/main/java/com/io/assignment/config/ApplicationConfig.java
@@ -3,12 +3,18 @@ package com.io.assignment.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class ApplicationConfig {
 
     @Bean
-    ModelMapper modelMapper() {
+     ModelMapper modelMapper() {
         return new ModelMapper();
+    }
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
