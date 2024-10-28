6b07c9d71406a79f7663bfa0c2d57b1afbfdc586
Md Shad Ahmad <136076679+mdshadahmad160@users.noreply.github.com>
2024-10-28 17:05:07 +0530

    updated_category_api

--- src/main/java/com/io/assignment/controller/BlogController.java
+++ src/main/java/com/io/assignment/controller/BlogController.java
@@ -1,17 +1,24 @@
 package com.io.assignment.controller;
 
+import com.io.assignment.payload.request.BlogRequest;
 import com.io.assignment.payload.request.CommentRequest;
+import com.io.assignment.payload.response.BlogResponse;
 import com.io.assignment.payload.response.CommentResponse;
+import com.io.assignment.payload.response.PageResponse;
 import com.io.assignment.security.CurrentUser;
 import com.io.assignment.security.UserPrincipal;
 import com.io.assignment.service.BlogService;
 import com.io.assignment.service.CommentService;
+import com.io.assignment.utils.AppConstant;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+/**
+ * @author Shad Ahmad
+ * @apiNote this could be the Blog Controller
+ */
 @RestController
 @RequestMapping("/api/v1/blogs")
 @RequiredArgsConstructor
@@ -28,5 +35,39 @@ public class BlogController {
         CommentResponse commentResponse = commentService.addComment(blogId, commentRequest, userPrincipal);
         return new ResponseEntity<>(commentResponse, HttpStatus.CREATED);
     }
+
+    @PostMapping
+    public ResponseEntity<BlogResponse> addBlog(@RequestBody BlogRequest blogRequest,
+                                                @CurrentUser UserPrincipal userPrincipal) {
+        BlogResponse blogResponse = blogService.addBlog(blogRequest, userPrincipal);
+        return new ResponseEntity<>(blogResponse, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<PageResponse<BlogResponse>> getAllBlogs(
+            @RequestParam(value = "page", required = false, defaultValue = AppConstant.DEFAULT_PAGE_NUMBER) Integer page,
+            @RequestParam(value = "size", required = false, defaultValue = AppConstant.DEFAULT_PAGE_SIZE) Integer size
+    ) {
+        PageResponse<BlogResponse> pageResponse = blogService.getAllBlogs(page, size);
+        return new ResponseEntity<>(pageResponse, HttpStatus.OK);
+    }
+
+    @GetMapping("/{blog_id}")
+    public ResponseEntity<BlogResponse> getBlogById(@PathVariable("blog_id") Long blogId) {
+        BlogResponse blogResponse = blogService.getBlogsById(blogId);
+        return new ResponseEntity<>(blogResponse, HttpStatus.OK);
+    }
+
+    @GetMapping("/{blog_id}/comments")
+    public ResponseEntity<PageResponse<CommentResponse>> getCommentsByBlog(
+            @PathVariable("blog_id") Long blogId,
+            @RequestParam(value = "page", required = false, defaultValue = AppConstant.DEFAULT_PAGE_NUMBER) Integer page,
+            @RequestParam(value = "size", required = false, defaultValue = AppConstant.DEFAULT_PAGE_SIZE) Integer size
+    ) {
+        PageResponse<CommentResponse> pageResponse = commentService.getCommentByBlog(blogId, page, size);
+
+        return new ResponseEntity<>(pageResponse, HttpStatus.OK);
+    }
+
 
 }
