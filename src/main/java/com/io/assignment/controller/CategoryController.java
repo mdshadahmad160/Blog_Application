0d73ee18183386ece8d03f978d3e5c50e51a06d8
Md Shad Ahmad <136076679+mdshadahmad160@users.noreply.github.com>
2024-10-28 12:18:23 +0530

    get_category_api
    this api will called by categoryId

--- src/main/java/com/io/assignment/controller/CategoryController.java
+++ src/main/java/com/io/assignment/controller/CategoryController.java
@@ -5,14 +5,10 @@ import com.io.assignment.payload.response.PageResponse;
 import com.io.assignment.service.BlogService;
 import com.io.assignment.service.CategoryService;
 import com.io.assignment.utils.AppConstant;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author Shad Ahmad
@@ -35,6 +31,12 @@ public class CategoryController {
         PageResponse<CategoryResponse> categoryResponse = categoryService.getAllCategories(page, size);
 
         return new ResponseEntity<>(categoryResponse, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{category_id}")
+    public ResponseEntity<CategoryResponse> getCategoryById(@PathVariable("category_id") Long categoryId) {
+        CategoryResponse categoryResponse = categoryService.getCategoryById(categoryId);
+        return new ResponseEntity<>(categoryResponse, HttpStatus.OK);
     }
 
 
