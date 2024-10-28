0a1148ed20ca83d79e42239a34ac64416f1cae53
Md Shad Ahmad <136076679+mdshadahmad160@users.noreply.github.com>
2024-10-28 12:07:38 +0530

    category api

--- src/main/java/com/io/assignment/controller/CategoryController.java
+++ src/main/java/com/io/assignment/controller/CategoryController.java
@@ -1,10 +1,22 @@
 package com.io.assignment.controller;
 
+import com.io.assignment.payload.response.CategoryResponse;
+import com.io.assignment.payload.response.PageResponse;
+import com.io.assignment.service.BlogService;
 import com.io.assignment.service.CategoryService;
+import com.io.assignment.utils.AppConstant;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * @author Shad Ahmad
+ */
 @RestController
 @RequestMapping("/api/v1/categories")
 @RequiredArgsConstructor
@@ -12,6 +24,18 @@ public class CategoryController {
 
 
     private final CategoryService categoryService;
+
+    private final BlogService blogService;
+
+    @GetMapping
+    public ResponseEntity<PageResponse<CategoryResponse>> getAllCategories(
+            @RequestParam(value = "page", defaultValue = AppConstant.DEFAULT_PAGE_NUMBER) Integer page,
+            @RequestParam(value = "size", defaultValue = AppConstant.DEFAULT_PAGE_SIZE) Integer size
+    ) {
+        PageResponse<CategoryResponse> categoryResponse = categoryService.getAllCategories(page, size);
+
+        return new ResponseEntity<>(categoryResponse, HttpStatus.CREATED);
+    }
 
 
 }
